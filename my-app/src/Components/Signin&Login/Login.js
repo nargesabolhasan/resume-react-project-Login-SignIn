620f515bd84f6bbd59fa561e761efca815562934
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-04-11 13:17:40 +0430

    useFormik for login

--- my-app/src/Components/Signin&Login/Login.js
+++ my-app/src/Components/Signin&Login/Login.js
@@ -2,45 +2,47 @@ import { React, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShowPassword from '../ShowPassword/ShowPassword';
+import { useFormik } from 'formik';
 
-const Login = ({ parentCallback }) => {
-    const [value, setValue] = useState({
-        email: '',
-        password: ''
-    })
+const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
 
     //
     return (
         <div className="col-8 mx-auto">
             <h1 className="text-center">خوش آمدید</h1>
-            <Form
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    parentCallback(value);
-                }}
-            >
+            <Form onSubmit={formik.handleSubmit} >
                 <Form.Group className="mb-3" >
                     <Form.Control
+                        id="email"
+                        name="email"
                         className="text-end inputs"
                         type="email"
                         placeholder="پست الکترونیک"
-                        value={value.email}
-                        onChange={(e) => setValue(prev => ({ ...prev, email: e.target.value }))}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
                         required
                     />
                 </Form.Group>
                 <ShowPassword
-                    value={value.password}
-                    onChange={(e) => setValue(prev => ({ ...prev, password: e.target.value }))
-                    } />
+                    value={formik.values.password}
+                    onChange={formik.handleChange} />
 
-                    <Button className="forget ">فراموش کردید؟</Button>
+                <Button className="forget ">فراموش کردید؟</Button>
                 <Button className="col-12 buttons" variant="primary" type="submit">
                     ورود
                 </Button>
             </Form>
         </div>
-    )
+    );
 }
 
 export default Login
